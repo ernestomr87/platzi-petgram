b6086abb3372321f1e06f41825107cd1ea91ada5
Ernesto M Rodriguez Rodriguez <ernestomr87@gmail.com>
2019-08-07 12:50:37 -0400

    platzy-class-10

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { ListOfCategories } from './components/ListOfCategories'
+import { GlobalStyle } from './GlobalStyles'
+
+export const App = () => {
+  return <div><GlobalStyle /><ListOfCategories /></div>
+}
