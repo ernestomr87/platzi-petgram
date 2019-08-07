332c7bc90d6d13f3c5eb2f4df34e167180f922df
Ernesto M Rodriguez Rodriguez <ernestomr87@gmail.com>
2019-08-07 15:34:35 -0400

    platzi-class-

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import { PhotoCard } from './../PhotoCard'
+import { PhotoCard } from '../PhotoCard'
 
 export const ListOfPhotoCards = () => {
-  return (<ul>
-    {[1, 2, 3, 4, 5, 6, 7, 8].map((photoCard) => {
-      return <li key={photoCard}><PhotoCard /></li>
-    })}
-
-  </ul>
+  return (
+    <ul>
+      {[1, 2, 3, 4, 5, 6, 7].map(id => <PhotoCard key={id} id={id} />)}
+    </ul>
   )
 }
